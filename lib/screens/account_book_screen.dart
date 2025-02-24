0f864ede2259308fd17ce6df0c9484958c9fee73
IBlackgoatI <paul6101301@gmail.com>
2025-02-25 04:24:46 +0900

    바텀 네비게이션 각 화면

---
+++ lib/screens/account_book_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountBookScreen extends StatelessWidget {
+  const AccountBookScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '가계부 화면',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
---
+++ lib/screens/all_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllScreen extends StatelessWidget {
+  const AllScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '전체 화면',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
